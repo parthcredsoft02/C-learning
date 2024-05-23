b5c088b9e358465c1c358c5681cc93eb80de826b
Parth CredSoft <163422318+parthcredsoft02@users.noreply.github.com>
2024-05-23 17:02:03 +0530

    Add files via upload

---
+++ firstprogram/firstprogram/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace firstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello parth");
+
+            // Variables
+            /* int x; //declaration 
+             x = 123; //initialization
+
+             int y;
+             y = 321;
+             Console.WriteLine(x+ y);
+             Console.WriteLine(y-x);
+             Console.WriteLine(y/x);
+             Console.WriteLine(x*y);
+
+             int age = 22; //This Can Store Full Number
+             Console.WriteLine("Your Age Is " + age);
+
+             double height = 300.5; //This Can Store Decimal Number
+             Console.WriteLine("Your Height is " + height +"Cm");
+
+
+             bool alive = true; //This Can Only Display True/False
+             Console.WriteLine("Are You Alive "+alive);
+             
+
+            char symbole = '@'; //This Can Only Store 1 Character
+            //Console.WriteLine("Your Symbole is "+symbole);
+            
+
+            string youtube = "BooIsLive02";
+            string owerner = "parth"; //This Can Store Whole String
+            String yt= symbole + youtube;
+            Console.WriteLine("Find Me On "+ yt +" This Channel Owerner is "+ owerner);
+
+            //Constances = Immutable values which are known at compile time
+            //              and not to change for the life of the program
+            // For Ex Pie=3.14159 We Cant Change It's Value
+
+            const double pi = 3.14159;
+            Console.WriteLine(pi);
+            
+
+
+            //Type Casting = Converting a Value into a different Data Type
+            //               Useful When We Accept User Input (String)
+            //               Differnet Data Types Can Do Different Things
+
+            double a = 3.14159;
+            int b = Convert.ToInt32(a); //We Will Use Int32 to Convert into intiger
+            Console.WriteLine(b);
+            */
+
+
+            Console.ReadKey();
+        }
+    }
+}
